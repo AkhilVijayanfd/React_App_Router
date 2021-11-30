79e8d88075275c2a8db752f50524009ee325f0e5
AkhilVijayanfd <92812336+AkhilVijayanfd@users.noreply.github.com>
2021-11-30 19:18:34 +0530

    Updated_testing
    Simple changes in existing project

--- src/Home.test.js
+++ src/Home.test.js
@@ -2,12 +2,10 @@
 
 import Home from "./Pages/Home/Home";
 
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
-// describe('Shallow Home Page',() =>{
+describe('Shallow Home Page',() =>{
 
-    it ('Object check', () => {
-        let wrapper = shallow(<Home/>);
-        expect(wrapper.exists('.first')).toEqual(true);
-    })
-// });
+        const wrapper = mount(<Home/>);
+        expect(wrapper).toContainElement('.first');
+});
